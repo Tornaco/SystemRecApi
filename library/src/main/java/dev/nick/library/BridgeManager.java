587c46ca01e474aa274eec813b3caee2b4ed00fa
NickGuo <guohao4@lenovo.com>
2017-07-28 10:09:55 +0800

    Add api to check if it is installed in system

--- library/src/main/java/dev/nick/library/BridgeManager.java
+++ library/src/main/java/dev/nick/library/BridgeManager.java
@@ -9,10 +9,11 @@ import lombok.experimental.var;
  * Created by Tornaco on 2017/7/24.
  * Licensed with Apache.
  */
-
 public enum BridgeManager {
 
     Instance;
+
+    static final String PKG_NAME = "dev.nick.systemrecapi";
 
     public static BridgeManager getInstance() {
         return Instance;
@@ -21,10 +22,40 @@ public enum BridgeManager {
     public boolean isInstalled(Context context) {
         try {
             var packageInfo = context.getPackageManager().getPackageInfo(
-                    "dev.nick.systemrecapi", 0);
+                    PKG_NAME, 0);
             return packageInfo != null;
         } catch (PackageManager.NameNotFoundException e) {
             return false;
+        }
+    }
+
+    public boolean isInstalledInSystem(Context context) {
+        try {
+            var packageInfo = context.getPackageManager().getPackageInfo(
+                    PKG_NAME, 0);
+            return packageInfo != null && packageInfo.sharedUserId.equals("android.uid.system");
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    public String getVersionName(Context context) {
+        try {
+            var packageInfo = context.getPackageManager().getPackageInfo(
+                    PKG_NAME, 0);
+            return packageInfo != null ? packageInfo.packageName : null;
+        } catch (PackageManager.NameNotFoundException e) {
+            return null;
+        }
+    }
+
+    public int getVersionCode(Context context) {
+        try {
+            var packageInfo = context.getPackageManager().getPackageInfo(
+                    PKG_NAME, 0);
+            return packageInfo != null ? packageInfo.versionCode : -1;
+        } catch (PackageManager.NameNotFoundException e) {
+            return -1;
         }
     }
 }
