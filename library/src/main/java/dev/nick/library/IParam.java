1bb115d5f8b7188b2c8304a90542e91afa54280a
NickGuo <guohao4@lenovo.com>
2017-07-28 13:13:49 +0800

    Add more params

--- library/src/main/java/dev/nick/library/IParam.java
+++ library/src/main/java/dev/nick/library/IParam.java
@@ -18,10 +18,15 @@ import lombok.ToString;
 @ToString
 public class IParam implements Parcelable {
 
+    // Audio
     private int audioSource;
+    private int audioBitrate;
+    private int audioSampleRate;
+
+    // Video
     private int orientation;
     private int frameRate;
-
+    private int iFrameInterval;
     private String resolution;
 
     private boolean shutterSound;
@@ -36,14 +41,18 @@ public class IParam implements Parcelable {
 
     protected IParam(Parcel in) {
         audioSource = in.readInt();
+        audioBitrate = in.readInt();
+        audioSampleRate = in.readInt();
         orientation = in.readInt();
         frameRate = in.readInt();
+        iFrameInterval = in.readInt();
         resolution = in.readString();
         shutterSound = in.readByte() != 0;
         stopOnScreenOff = in.readByte() != 0;
         stopOnShake = in.readByte() != 0;
         useMediaProjection = in.readByte() != 0;
         showNotification = in.readByte() != 0;
+        showTouch = in.readByte() != 0;
         path = in.readString();
     }
 
@@ -67,14 +76,18 @@ public class IParam implements Parcelable {
     @Override
     public void writeToParcel(Parcel parcel, int i) {
         parcel.writeInt(audioSource);
+        parcel.writeInt(audioBitrate);
+        parcel.writeInt(audioSampleRate);
         parcel.writeInt(orientation);
         parcel.writeInt(frameRate);
+        parcel.writeInt(iFrameInterval);
         parcel.writeString(resolution);
         parcel.writeByte((byte) (shutterSound ? 1 : 0));
         parcel.writeByte((byte) (stopOnScreenOff ? 1 : 0));
         parcel.writeByte((byte) (stopOnShake ? 1 : 0));
         parcel.writeByte((byte) (useMediaProjection ? 1 : 0));
         parcel.writeByte((byte) (showNotification ? 1 : 0));
+        parcel.writeByte((byte) (showTouch ? 1 : 0));
         parcel.writeString(path);
     }
 }
